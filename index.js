9ca4171dda882ce0991fe5e10ce468e5d0e8fde8
FO Partner <140566293+Hovup-FO@users.noreply.github.com>
2024-02-27 12:53:37 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const dateSnapshot = new Date();
+const year = dateSnapshot.getFullYear();
+const copyrightText = `Todos los derechos reservados, Copyright: &copy;${year}, Hovup, Inc.`;
+
+document.getElementById('copyright').innerHTML = copyrightText;
+
+
+
