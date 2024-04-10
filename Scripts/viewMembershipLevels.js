fb8f506668589cd7907b5a3d8a97116b57c12d64
FO Partner <140566293+Hovup-FO@users.noreply.github.com>
2024-04-10 00:07:23 -0600

    Update viewMembershipLevels.js

--- Scripts/viewMembershipLevels.js
+++ Scripts/viewMembershipLevels.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const accessToken = localStorage.getItem('accessToken');
     const userId = localStorage.getItem('userId');
-   
     const membershipLevelsContainer = document.getElementById('membershipLevels');
 
     if (!userId || !accessToken) {
